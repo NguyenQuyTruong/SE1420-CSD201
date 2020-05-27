eb6abd3ff3328304ea7100f0f1d892e25047b396
Tiểu Minh <tieuminh2510@gmail.com>
2020-05-27 19:26:12 +0700

    PriorityQueue Implement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,17 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Entry A = new Entry(0, "A");
-        Entry B = new Entry(1, "B");
-        Entry C = new Entry(2, "B");
-        PriorityQueue queue = new PriorityQueue();
-        queue.enqueue(A);
-        queue.enqueue(B);
-        queue.enqueue(C);
-        Entry e = queue.dequeue();
-        e.set_rank(5);
+        Entry A = new Entry(2, "A");
+        Entry B = new Entry(-1, "B");
+        Entry C = new Entry(-1, "C");
+        SortedPriorityQueue queue = new SortedPriorityQueue();
+        queue.insert(A);
+        queue.insert(B);
+        queue.insert(C);
 
-        System.out.print(A);
+        System.out.print(queue);
+        queue.removeMin();
+        System.out.print(queue);
+//        System.out.print(queue.removeMin());
     }
 }
--- src/com/company/PriorityQueue.java
+++ src/com/company/PriorityQueue.java
@@ -1,28 +1,11 @@
 package com.company;
+
 import com.company.Entry;
-//import com.company.SinglyLinkedList;
 
-public class PriorityQueue {
-    private SinglyLinkedList<Entry> _list = new SinglyLinkedList<Entry>();
-    public void enqueue(Entry e) {
-        _list.addLast(e);
-    }
-
-    public Entry dequeue() {
-        return _list.removeFirst();
-    }
-    public Entry first() {
-        return _list.getTop();
-    }
-    public boolean isEmpty() {
-        return _list.isEmpty();
-    }
-    public int size() {
-        return  _list.size();
-    }
-
-    @Override
-    public String toString() {
-        return _list.toString();
-    }
+public interface PriorityQueue {
+    public Entry removeMin();
+    public Entry min();
+    public boolean isEmpty();
+    public int size();
+    public String toString();
 }
