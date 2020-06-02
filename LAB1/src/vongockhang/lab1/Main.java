be65b059c0cf248383cf35a18cd3c991c10c296f
skyho <khangzxrr@gmail.com>
2020-06-02 13:53:17 +0700

    init commit

---
+++ LAB1/src/vongockhang/lab1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package vongockhang.lab1;
+
+/**
+ * @author vo ngoc khang
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
