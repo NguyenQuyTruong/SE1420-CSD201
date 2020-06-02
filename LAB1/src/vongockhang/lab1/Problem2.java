179e90e7c3a4bc410cf63b1d42edefb5257b3efd
skyho <khangzxrr@gmail.com>
2020-06-02 20:58:25 +0700

    Add Compare tag method, processing normal tag

--- LAB1/src/vongockhang/lab1/Problem2.java
+++ LAB1/src/vongockhang/lab1/Problem2.java
@@ -2,10 +2,60 @@ package vongockhang.lab1;
 
 import java.io.IOException;
 
+import vongockhang.lab1.Stack.StackException;
+
 public class Problem2 {
 	Stack stack = new Stack(500); //init stack
+	CSV csv = new CSV();
+
 	
-	private void ParsingHTML(String body) {
+	
+	/**
+	 *  to compare 2 tags current close tag and top of stack tag
+	 *  
+	 * @param tag
+	 * @param stackTag
+	 * @return true if tag suitable with close tag
+	 */
+	private boolean compareTag(String tag, String stackTag) {
+		return false;
+	}
+	
+	/**
+	 * this method will simplify tags and put/get from stack to process it
+	 * some special tag will be skip (also count it)
+	 * @param tag current tag which need to process
+	 * @throws StackException 
+	 */
+	private void ProcessTag(String tag) throws StackException {
+		tag = tag.toLowerCase(); //make sure, test case can't trick us!
+		
+		switch(tag) {
+		case "<!doctype html>": 
+			
+		default:
+			break;
+		}
+		
+		if (tag.contentEquals("<!doctype html>")) {
+			csv.UpdateValue("<!doctype html>"); 
+			//this doctype tag is one of special, so we count it as a tag for easy process	
+		}else if (tag.contains("<meta")) {
+			csv.UpdateValue("<meta>");
+			//meta tag is another special!
+		}else { 
+			//fun part which needs stack for process
+			
+			if (tag.contains("</")) { //this is not a closing tag, put it to stack
+				stack.Push(tag);
+			}else { //This is Closting Tag condition, lets check it with the top of our Stack
+				
+			}
+			
+			System.out.println(tag);
+		}
+	}
+	private void ParsingHTML(String body) throws StackException {
 		System.out.println(body);
 		System.out.println("===================");
 		String tag = "";
@@ -26,7 +76,7 @@ public class Problem2 {
 				suckingCharacterToTag = false;  
 				//Turn this flag off, we already have enough characters to create a tag
 				
-				System.out.println(tag);
+				ProcessTag(tag);
 			}
 		}
 	}
@@ -43,6 +93,9 @@ public class Problem2 {
 			ParsingHTML(body);
 			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (StackException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
