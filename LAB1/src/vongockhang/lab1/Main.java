4ea469dd01ec5fb35b1a0c1a244b9b067f01c302
skyho <khangzxrr@gmail.com>
2020-06-02 16:20:19 +0700

    add argument parsing and init solver for problem 2

--- LAB1/src/vongockhang/lab1/Main.java
+++ LAB1/src/vongockhang/lab1/Main.java
@@ -11,10 +11,32 @@ public class Main {
 
 	/**
 	 * @param args
-	 * 
+	 * args[0]: problem 1 or 2
+	 * if problem 2: 
+	 * 		+ args[1]: url of website
+	 * 		+ args[2]: csv file path
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		try {
+			switch (Integer.parseInt(args[0])) {
+			case 1:
+				break;
+			case 2:
+				if (args.length < 3) {
+					throw new IllegalArgumentException("Not enough argument for problem 2");
+				}
+				Problem2 problem = new Problem2(args[1], args[2]);
+				break;
+			default:
+				break;	
+			}
+		}catch(NumberFormatException e) {
+			System.out.println("Error at ARGS[0]: Please choose problem number 1 or 2 ONLY!");
+		}catch(IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
 	}
 
 }
