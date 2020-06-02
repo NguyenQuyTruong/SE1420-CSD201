bcbdc96681c209195b254650281f0f2fdb146dc2
Khang Ngoc <khangzxrr@gmail.com>
2020-06-02 16:59:54 +0700

    Finish basic method of getting tags

--- LAB1/src/vongockhang/lab1/Problem2.java
+++ LAB1/src/vongockhang/lab1/Problem2.java
@@ -7,9 +7,27 @@ public class Problem2 {
 	
 	private void ParsingHTML(String body) {
 		System.out.println(body);
+		System.out.println("===================");
+		String tag = "";
 		
+		boolean suckingCharacterToTag = false;
 		for(int i = 0; i < body.length(); i++) { //looping though any single character
-			
+			if(body.charAt(i) == '<') { 
+				//if open bracket then find close braket compile it => a TAG
+				tag = "<";
+				suckingCharacterToTag = true; 
+				//lol, this flag will let us know we're collecting characters into a TAG 
+				
+			}else if ((body.charAt(i) != '>') && (suckingCharacterToTag)){
+				tag += body.charAt(i); //normal character, add it to our incomplete tag
+				
+			}else if ((body.charAt(i) == '>') && (suckingCharacterToTag)) {
+				tag += '>'; //close tag => end of tag
+				suckingCharacterToTag = false;  
+				//Turn this flag off, we already have enough characters to create a tag
+				
+				System.out.println(tag);
+			}
 		}
 	}
 	/**
