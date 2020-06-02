67f862c693f839b9463a0a50c50d09b1645448e0
skyho <khangzxrr@gmail.com>
2020-06-02 14:36:50 +0700

    Add stack constructor

---
+++ LAB1/src/vongockhang/lab1/Stack.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package vongockhang.lab1;
+
+/**
+ * @author skyho
+ *
+ */
+public class Stack implements IStack{
+	int top;
+	int[] storage;
+	
+	/**
+	 * Stack constructor
+	 * @param capacity init the storage limit or the length of stack
+	 */
+	public Stack(int capacity) {
+		
+	}
+	
+	/**
+	 * add an HTML tag to stack
+	 * @param tag This tag will be added to stack 
+	 * */
+	@Override
+	public void Push(String tag) {
+		
+		
+	}
+
+	@Override
+	public String Pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String GetTop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean IsEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
