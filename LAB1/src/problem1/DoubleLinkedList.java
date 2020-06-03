6edbb32fd4fe5ebf0c1697c678662b50de97d451
skyho <khangzxrr@gmail.com>
2020-06-03 18:41:11 +0700

    add makeNode method

--- LAB1/src/problem1/DoubleLinkedList.java
+++ LAB1/src/problem1/DoubleLinkedList.java
@@ -1,11 +1,27 @@
 package problem1;
 
 public class DoubleLinkedList {
-	private class node{
+	 class node{
 		node left = null;
 		node right = null;
 		String email;
 		long point;
+		
+		public node(String email, long point) {
+			this.email = email;
+			this.point = point;
+		}
+	}
+	 
+	 /**
+	  * create new node with email, point
+	  * @param email
+	  * @param point
+	  * @return new node
+	  */
+	private node makeNode(String email, long point) {
+		node newNode = new node(email, point);
+		return newNode;
 	}
 	
 	node head;
@@ -17,7 +33,7 @@ public class DoubleLinkedList {
 		head = null; //init head is null
 	}
 	
-	public void insert() {
+	public void insert(String email, long point) {
 		
 	}
 }
