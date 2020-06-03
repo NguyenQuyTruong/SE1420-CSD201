b38fbd89d67a07c138d1cf8da308c5969447a2b2
skyho <khangzxrr@gmail.com>
2020-06-03 18:36:50 +0700

    add constructor, incomplete insert method

--- LAB1/src/problem1/DoubleLinkedList.java
+++ LAB1/src/problem1/DoubleLinkedList.java
@@ -1,5 +1,23 @@
 package problem1;
 
 public class DoubleLinkedList {
-
+	private class node{
+		node left = null;
+		node right = null;
+		String email;
+		long point;
+	}
+	
+	node head;
+	/**
+	 * DoubleLinkedList constructor, first we will set head to null (make sure) 
+	 * 
+	 */
+	public DoubleLinkedList() {
+		head = null; //init head is null
+	}
+	
+	public void insert() {
+		
+	}
 }
--- LAB1/src/problem1/PriorityQueue.java
+++ LAB1/src/problem1/PriorityQueue.java
@@ -1,5 +1,5 @@
 package problem1;
 
 public class PriorityQueue {
-
+	
 }
