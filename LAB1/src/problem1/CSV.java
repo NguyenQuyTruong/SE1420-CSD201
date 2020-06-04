d0ae4dd2756a8905092ba1ad883e5bf18d594628
skyho <khangzxrr@gmail.com>
2020-06-04 16:21:17 +0700

    add write queue to csv method

--- LAB1/src/problem1/CSV.java
+++ LAB1/src/problem1/CSV.java
@@ -5,38 +5,51 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class CSV {
 	/**
 	 * Read csv file data and insert it to priority queue
+	 * 
 	 * @param queue
 	 * @param csvPath
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void ReadAndPushToQueue(PriorityQueue queue, String csvPath) throws IOException {
-		
+
 		BufferedReader bufferedReader = null;
 		String line = "";
-		
+
 		try {
 			bufferedReader = new BufferedReader(new FileReader(csvPath));
-			while((line = bufferedReader.readLine()) != null) {
-				String[] rowData = line.split(","); //split data into array using ","
-				if (rowData.length < 2) { //catch this error before it break your program
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] rowData = line.split(","); // split data into array using ","
+				if (rowData.length < 2) { // catch this error before it break your program
 					throw new IOException("Not enough data in a row (csv file)");
 				}
-				
+
 				String email = rowData[0];
 				String unparsedPoint = rowData[1];
-				long point =  Long.parseLong(unparsedPoint.trim());
+				long point = Long.parseLong(unparsedPoint.trim());
 				queue.insert(email, point);
 			}
-			
+
 			System.out.println("Finish reading csv file");
-			
-		}catch(FileNotFoundException e) {
+
+		} catch (FileNotFoundException e) {
 			System.out.println("Csv file not found, please check again!");
-		}finally {
+		} finally {
 			bufferedReader.close();
 		}
+	}
+
+	/**
+	 * write queue to csv file
+	 * @param queue
+	 * @param path
+	 * @throws IOException
+	 */
+	public static void WriteQueueIntoCSV(PriorityQueue queue, String path) throws IOException {
+		queue.writeCSV(path);
+
 	}
 }
