8fbbe26f32be340af09a9783be12dcd1e3fc5474
skyho <khangzxrr@gmail.com>
2020-06-04 07:37:03 +0700

    Implement callable for CLI

--- LAB1/src/problem1/Problem1.java
+++ LAB1/src/problem1/Problem1.java
@@ -1,8 +1,16 @@
 package problem1;
 
-public class Problem1 {
+import java.util.concurrent.Callable;
+
+public class Problem1 implements Callable<Integer>{
 	public Problem1() {
 		PriorityQueue queue = new PriorityQueue();
 		
+	}
+
+	@Override
+	public Integer call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
