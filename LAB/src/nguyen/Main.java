5e16463cd1b6319176cee15b4905e8f7935c4c33
ADMIN <ADMIN@SE141094>
2020-06-04 13:14:29 +0700

    Nguyen First Commit

--- LAB/src/nguyen/Main.java
+++ LAB/src/nguyen/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello GitHub");
+        System.out.println("nguyen khoi nguyen");
     }
     
 }
