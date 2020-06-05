833e739da74d312f6022989d29fc9cea3cc8d18d
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-05 07:53:29 +0700

    create DoubleLinkedList Class

--- LAB1/src/main/java/mobileGame/DoubleLinkedList.java
+++ LAB1/src/main/java/mobileGame/DoubleLinkedList.java
@@ -10,5 +10,42 @@ package mobileGame;
  * @author nhoxr
  */
 public class DoubleLinkedList {
-    
+    //create class Node
+    private class Node {
+	private Gamer gamer;
+	private Node right;
+	private Node left;
+	
+	//constructor
+	public Node(Gamer gamer, Node right, Node left) {
+	    this.gamer = gamer;
+	    this.right = right;
+	    this.left = left;
+	}
+
+	//getter and setter for Node
+	public Gamer getGamer() {
+	    return gamer;
+	}
+
+	public void setGamer(Gamer gamer) {
+	    this.gamer = gamer;
+	}
+
+	public Node getRight() {
+	    return right;
+	}
+
+	public void setRight(Node right) {
+	    this.right = right;
+	}
+
+	public Node getLeft() {
+	    return left;
+	}
+
+	public void setLeft(Node left) {
+	    this.left = left;
+	}
+    }
 }
