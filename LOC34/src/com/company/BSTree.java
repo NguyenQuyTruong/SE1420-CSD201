4b13d987799ea85a505aee1af68dc40efce29087
Thai Tieu Minh <tieuminh2510@gmail.com>
2020-06-06 07:35:31 +0700

    CLI & BSTree

--- LOC34/src/com/company/BSTree.java
+++ LOC34/src/com/company/BSTree.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class BSTree {
     private class Node {
         private int info;
@@ -9,4 +11,29 @@ public class BSTree {
     }
     private Node root;
     public BSTree() { root = null; };
+
+    private void visit(Node p) {
+        System.out.print(p.info + " ");
+    }
+
+    private void inOrder(Node p) {
+        if(p == null) return;
+        inOrder(p.left);
+        visit(p);
+        inOrder(p.right);
+    }
+    public void print() {
+        inOrder(root);
+        System.out.println("");
+    }
+    public void insert(int value) {
+        root = insertNode(value, root);
+    }
+    private Node insertNode(int value, Node p) {
+        if(p == null) p = new Node(value);
+        if(value > p.info) p.right = insertNode(value, p.right);
+        else if (value < p.info) p.left = insertNode(value, p.left);
+        return p;
+    }
+
 }
