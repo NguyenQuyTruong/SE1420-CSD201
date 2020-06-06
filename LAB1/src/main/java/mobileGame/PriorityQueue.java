1efc42fca4c6c77a62fa65c9c464071c8796dbd3
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-06 16:02:43 +0700

    add push method

--- LAB1/src/main/java/mobileGame/PriorityQueue.java
+++ LAB1/src/main/java/mobileGame/PriorityQueue.java
@@ -10,5 +10,16 @@ package mobileGame;
  * @author nhoxr
  */
 public class PriorityQueue {
-    
+
+    private DoubleLinkedList list = new DoubleLinkedList();
+    /**
+     * this method will add user to the queue and sort it depend on their point
+     * from low to high
+     *
+     * @param data user
+     */
+    public void push(Gamer data) {
+	list.add(data);
+	System.out.println("push Gamer data successful!!");
+    }
 }
