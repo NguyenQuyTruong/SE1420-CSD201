34ca4d58bc337bf81f2825855d77e9aa83312a34
ADMIN <ADMIN@SE141094>
2020-06-07 17:25:39 +0700

    update Main

--- LAB/src/nguyen/Main.java
+++ LAB/src/nguyen/Main.java
@@ -24,6 +24,7 @@ public class Main {
         list.add("silver", 55);
         list.add("bach ho", 26);
         list.add("thanh long", 2222);
+        list.add("Thay Minh", 0);
         list.printsortList();
         System.out.println("==Search MaxPonit Player==");
         list.SearchMaxpointPlayer();
@@ -33,6 +34,8 @@ public class Main {
         list.printsortList();
         System.out.println("==Search Bach Ho==");
         System.out.println(list.SearchPlayerEmail("bach ho"));
+        System.out.println("==Search Thay Minh==");
+        System.out.println(list.SearchPlayerEmail("Thay Minh"));
         System.out.println("==Updata Nguyen Khoi Nguyen >>8888 point==");
         list.UpdatePlayer("Nguyen Khoi Nguyen", 8888);
         list.printsortList();
