ba3b37146209ae41072669fe4998647235c2e519
ADMIN <ADMIN@SE141094>
2020-06-07 11:56:55 +0700

    Main

--- LAB/src/nguyen/Main.java
+++ LAB/src/nguyen/Main.java
@@ -16,8 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("nguyen khoi nguyen");
-        System.out.println("Test");
+        SortLinkedList list = new SortLinkedList();
     }
     
 }
