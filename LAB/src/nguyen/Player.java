5b09c82fce8dd8fd06a731a0b1b0e6994f609b8f
ADMIN <ADMIN@SE141094>
2020-06-07 11:09:46 +0700

    Update Player

--- LAB/src/nguyen/Player.java
+++ LAB/src/nguyen/Player.java
@@ -14,17 +14,17 @@ public class Player {
     private String Point;
     Player nextPlayer;
     Player beforePlayer;
-
+//tao contruccstor khong tham so
     public Player() {
     }
-
+// constructor co tham so 
     public Player(String Email, String Point, Player nextPlayer, Player beforePlayer) {
         this.Email = Email;
         this.Point = Point;
         this.nextPlayer = nextPlayer;
         this.beforePlayer = beforePlayer;
     }
-
+// insert Get&&Set
     public String getEmail() {
         return Email;
     }
@@ -56,6 +56,7 @@ public class Player {
     public void setBeforePlayer(Player beforePlayer) {
         this.beforePlayer = beforePlayer;
     }
+    //Add method String to string 
     @Override
    public String toString(){
         return String.format("%-15s%-10d",Email, Point);
