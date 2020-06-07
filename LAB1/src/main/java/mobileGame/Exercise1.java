98a17e8c52766194ad55e91e58bb2b9a8d452924
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-07 14:13:42 +0700

    create insertNewGamer method

--- LAB1/src/main/java/mobileGame/Exercise1.java
+++ LAB1/src/main/java/mobileGame/Exercise1.java
@@ -10,7 +10,20 @@ package mobileGame;
  * @author nhoxr
  */
 public class Exercise1 {
-    
+
     PriorityQueue queue;
-    
+
+    /**
+     * this method use to add new Gamer to queue
+     * @param email
+     * @param point
+     */
+    public void insertNewGamer(String email, String point) {
+	try {
+	    int checkPoint = Integer.parseInt(point.trim());
+	    queue.push(new Gamer(email, checkPoint));
+	} catch (NumberFormatException e) {
+	    System.out.println("Point must be integer!!");
+	}
+    }
 }
