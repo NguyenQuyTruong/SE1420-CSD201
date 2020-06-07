c4abcd22ab2583e9bb5c73d0eb6c7f87e2bf4f38
ADMIN <ADMIN@SE141094>
2020-06-07 11:14:17 +0700

    Creat Class SortLinkedList

---
+++ LAB/src/nguyen/SortLinkedList.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nguyen;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SortLinkedList {
+    Player head;
+    Player tail;
+    public SortLinkedList(){
+        head=tail=null;
+    }
+}
