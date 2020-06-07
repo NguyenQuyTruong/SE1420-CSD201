6b1208266c161bd038e32838f9a35bb5c886101f
KHAM <KHAM@192.168.247.1>
2020-06-07 21:02:02 +0700

    Create MyQueue

---
+++ LAB1/src/main/java/MyQueue.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KHAM
+ */
+public class MyQueue {
+
+    private class Node {
+        private Entry user;
+        private Node next;
+
+        public Node(Entry user, Node next) {
+            this.user = user;
+            this.next = next;
+        }
+
+        public Node() {
+        }
+
+        public Entry getUser() {
+            return user;
+        }
+
+        public void setUser(Entry user) {
+            this.user = user;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+        
+        
+    }
+    
+    
+}
