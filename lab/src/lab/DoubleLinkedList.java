f0e5113bc5bb29e2cd4046487e3aeea8b452ee10
Khả Thi <Kh Thi@169.254.71.86>
2020-06-07 14:44:32 +0700

    2

---
+++ lab/src/lab/DoubleLinkedList.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab;
+
+/**
+ *
+ * @author Khả Thi
+ */
+public class DoubleLinkedList {
+    Node head,tail;
+    class Node{
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int d) {
+            data = d;
+        }
+        
+    }
+}
