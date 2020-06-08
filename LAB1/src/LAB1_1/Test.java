239f8d762a53a04defd46d0ecc9b06aeb501f31e
minhv <minhv@DESKTOP-FD4J87R>
2020-06-08 07:44:42 +0700

    Test all function

--- LAB1/src/LAB1_1/Test.java
+++ LAB1/src/LAB1_1/Test.java
@@ -11,27 +11,27 @@ package Lab1_1;
  */
 public class Test {
 
-    public static void main(String[] args) {
-	DoublyLinkedList list = new DoublyLinkedList();
-	User user1 = new User("123", 1);
-	User user2 = new User("294", 2);
-	User user3 = new User("345", 3);
-	User user4 = new User("533", 4);
-	User user5 = new User("341", 5);
-	User user6 = new User("435", 6);
-	User user7 = new User("325", 7);
-	User user8 = new User("652", 8);
-	User user9 = new User("342", 9);
-	User user10 = new User("546", 10);
+    public static void main(String[] args) throws Exception {
+	LAB1_1.PriorityQueue list = new LAB1_1.PriorityQueue();
+	User user1 = new User("1", 1);
+	User user2 = new User("2", 2);
+	User user3 = new User("3", 3);
+	User user4 = new User("4", 4);
+	User user5 = new User("5", 5);
+	User user6 = new User("6", 6);
+	User user7 = new User("7", 7);
+	User user8 = new User("8", 8);
+	User user9 = new User("9", 9);
+	User user10 = new User("10", 10);
 
-	list.addFirst(user5);
-	list.addFirst(user3);
-	list.addFirst(user1);
-	list.addLast(user6);
-	list.addLast(user9);
-	list.add(user8);
-//	list.remove(list.searchUserByEmail("123"));
+	list.insertUser(user1);
+	list.insertUser(user3);
+	list.insertUser(user6);
+	list.insertUser(user2);
+	list.insertUser(user5);
+	list.deleteUser("126");
+	list.updateUser("345", 2);
 
-	list.printlist(list);
+	list.displayUser();
     }
 }
