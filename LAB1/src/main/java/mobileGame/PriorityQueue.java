c28f16b120776028be40af0fd1e150b720f79690
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-08 12:45:27 +0700

    move readFile method

--- LAB1/src/main/java/mobileGame/PriorityQueue.java
+++ LAB1/src/main/java/mobileGame/PriorityQueue.java
@@ -5,6 +5,10 @@
  */
 package mobileGame;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 /**
  *
@@ -79,6 +83,39 @@ public class PriorityQueue {
 	}
     }
 
+    /**
+     * this method use to Read the file and import Gamer data(email, point) to the queue
+     * @param queue
+     * @param fileName 
+     */
+    public void ReadFile(PriorityQueue queue, String fileName) {
+	FileReader fr = null;
+	BufferedReader bf = null;
+	try {
+	    fr = new FileReader(fileName);
+	    bf = new BufferedReader(fr);
+	    while (bf.ready()) {
+		String s = bf.readLine();
+		String[] arr = s.split(", ");
+		if (!(arr[1].equalsIgnoreCase("point"))) {
+		    queue.push(new Gamer(arr[0], Integer.valueOf(arr[1])));
+		}
+	    }
+	} catch (IOException e) {
+	    System.out.println("File not exist!!");
+	} finally {
+	    try {
+		if (fr != null) {
+		    fr.close();
+		}
+		if (bf != null) {
+		    bf.close();
+		}
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+    }
     //write file method
     public void writeFile(String fileName) {
 	list.writeToCSVfile(fileName);
