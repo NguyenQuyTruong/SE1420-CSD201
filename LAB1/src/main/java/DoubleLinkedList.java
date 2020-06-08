42199520e8a559c9da994a50b501df5f46b1d1ed
redra <redra@26.12.181.22>
2020-06-08 22:13:09 +0700

    LinkedList

---
+++ LAB1/src/main/java/DoubleLinkedList.java
@@ -0,0 +1,13 @@
+
+public class DoubleLinkedList {
+    //Create Node
+    public class Node{
+        private Player player;
+        private Node right;
+        private Node left;
+
+        
+        
+    }
+    
+}
