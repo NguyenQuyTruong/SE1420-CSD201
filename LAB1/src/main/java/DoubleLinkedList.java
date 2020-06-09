d20361b51fbe19e44cc1d43f0326d1187c4ceae5
redra <redra@26.12.181.22>
2020-06-09 12:35:11 +0700

    LinkedList

--- LAB1/src/main/java/DoubleLinkedList.java
+++ LAB1/src/main/java/DoubleLinkedList.java
@@ -1,46 +1,9 @@
 
 public class DoubleLinkedList {
-    //Create Node
+        //Create Node
     public class Node{
         private Player player;
         private Node right;
         private Node left;
-
-        //Contructor + Setter + Getter...
-        public Node(Player player, Node right, Node left){
-            this.player = player;
-            this.left = left;
-            this.right = right;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public void setPlayer(Player player) {
-            this.player = player;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
-        }
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public Player getPlayer() {
-            return player;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-        
     }
-    //Create Main Nodes
-    public Node Head;
-    public Node Tail;
-    public int nodeSize = 0;
-    
 }
