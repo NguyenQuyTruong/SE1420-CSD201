fe3aafe33752a3bbd3df85da17db8847b7d87581
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-09 22:27:23 +0700

    add some method for stack

--- Lab1/Lab1/src/HTMLtags/Stack.java
+++ Lab1/Lab1/src/HTMLtags/Stack.java
@@ -10,5 +10,28 @@ package HTMLtags;
  * @author nhoxr
  */
 public class Stack {
+    public static final int CAPACITY = 1000; //default arrya capacity
+    private String[] tag; //data type of tag html
+    private int t = -1; //index of the top element in stack
+
+    public Stack(int capacity) {
+	tag = new String[capacity];
+    }
+    
+    /**
+     * this method use to return size of the stack
+     * @return size of stack
+     */
+    public int size() {
+	return (t + 1);
+    }
+    
+    /**
+     * this method use to check is the stack empty or not
+     * @return true or false
+     */
+    public boolean isEmpty() {
+	return (t == -1);
+    }
     
 }
