0129074d015f22b4a0a6a44c37f35bcfc467ec1e
ducthang1410 <thangnguyen14109@gmail.com>
2020-06-09 19:16:40 +0700

    Add Summary

--- LAB1/Problem1/src/DoubleLinkedList.java
+++ LAB1/Problem1/src/DoubleLinkedList.java
@@ -9,11 +9,23 @@
  * @author USER
  */
 public class DoubleLinkedList {
+    /**
+    * Create an node of using doubly linkedlist
+    *
+    * @param <User>
+    */
     private class Node{
         private User data;
         private Node next;
         private Node pre;
 
+        /**
+	 * Create constructor
+	 *
+	 * @param data
+	 * @param next
+	 * @param prev
+	 */
         public Node(User data, Node next, Node pre) {
             this.data = data;
             this.next = next;
@@ -24,12 +36,23 @@ public class DoubleLinkedList {
     
     private Node header;
     private Node trailer;
+    
+    /**
+     * Constructs a new empty list.
+     */
     public DoubleLinkedList() {
         header = new Node(null, null, null);
-        trailer = new Node(null, null, null);
+        trailer = new Node(null, null, header);
         header.next = trailer;
     }
     
+    /**
+     * Add new node
+     *
+     * @param data
+     * @param nextNode
+     * @param prevNode
+     */
     private void insert(User data, Node front, Node back){
         Node n = new Node(data, back, front);
         front.next = n;
