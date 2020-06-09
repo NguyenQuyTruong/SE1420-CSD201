e8a2b13f83fa0f361fbc9d8e43751a5fbbbd98a9
ACER <ACER@SE140264>
2020-06-09 16:27:54 +0700

    alo alo alo

--- SE140264/src/se140264/SE140264.java
+++ SE140264/src/se140264/SE140264.java
@@ -16,6 +16,7 @@ public class SE140264 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("imma do it later!");
     }
     
 }
