983c598956fda5834a9039ffbf8a96cefffe2c51
ACER <ACER@192.168.203.1>
2020-06-09 22:21:02 +0700

    Test game

--- LAB1/src/DoubleLinkedList.java
+++ LAB1/src/DoubleLinkedList.java
@@ -1,3 +1,7 @@
+
+import javax.xml.soap.Node;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,44 +13,45 @@
  * @author ACER
  */
 public class DoubleLinkedList {
-    private class Node{ //node class
-        int data ; //data user
-        Node prev;
-        Node next;
-        
-        public Node(int data, Node prev_node, Node next_node){
-           this.data = data;
-           this.prev = prev_node;
-           this.next = next_node;
-        }
+    private class Node{
+        private int data;
+        private Node prev;
+        private Node next;
 
-        public Node() {
-        }
-
-        public int getData() {
-            return data;
-        }
-
-        public void setData(int data) {
+        public Node(int data, Node prev, Node next) {
             this.data = data;
-        }
-
-        public Node getPrev() {
-            return prev;
-        }
-
-        public void setPrev(Node prev) {
             this.prev = prev;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
             this.next = next;
         }
         
+}
+    
+    private Node head;
+    private Node tail;
+        public DoubleLinkedList(){
+          head = tail = null;
+        }
         
+        public boolean isEmpty(){
+          return head == null;
+        //append a new node to the end of list
+    }
+        
+        public void insert(int x, Node font, Node back){
+            Node node = new Node(x, font, back);
+            font.next = node;
+            back.prev = node;
+        }
+        
+        public void addFirst(int z){
+            insert(z, head.next, head);
+        }
+        public void addLast(int y){
+            insert(y, tail.prev, tail);
+            
+    }
+
+        public int size(){
+            return 0;
     }
 }
