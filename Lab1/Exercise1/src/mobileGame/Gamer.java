fedae757052ee1196751b31b7472ec297b106a3e
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-09 09:46:53 +0700

    update new folder

---
+++ Lab1/Exercise1/src/mobileGame/Gamer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mobileGame;
+
+/**
+ *
+ * @author nhoxr
+ */
+public class Gamer{
+
+    //gamer has email and point
+    private String email;
+    private int point;
+
+    public Gamer() {
+    }
+
+    public Gamer(String email, int point) {
+	this.email = email;
+	this.point = point;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public int getPoint() {
+	return point;
+    }
+
+    public void setPoint(int point) {
+	this.point = point;
+    }
+
+    //print info method
+    @Override
+    public String toString() {
+	return "Gamer{" + "email=" + email + ", point=" + point + '}';
+    }
+}
