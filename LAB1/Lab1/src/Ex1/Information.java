a6f4d48d1879295db3f68bb6112ed03d18be0d47
Thúy Bắc <Thy Bc@SE140333>
2020-06-10 14:48:33 +0700

    Update

---
+++ LAB1/Lab1/src/Ex1/Information.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex1;
+
+/**
+ *
+ * @author Thúy Bắc
+ */
+public class Information {
+    String email;
+    int point;
+
+    public Information(String email, int point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return "Information{" + "email = " + email + ", point = " + point + '}';
+    }
+}
