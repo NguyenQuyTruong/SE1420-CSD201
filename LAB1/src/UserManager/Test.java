465809bd2ca4f878e8a84b2ed934cf535d5684d6
minhv <minhv@DESKTOP-FD4J87R>
2020-06-11 03:50:00 +0700

    Fix some bug

--- LAB1/src/UserManager/Test.java
+++ LAB1/src/UserManager/Test.java
@@ -5,6 +5,7 @@
  */
 package UserManager;
 
+import HTMLParser.HTMLValidateAndCount;
 import HTMLParser.Stack;
 import java.util.Scanner;
 
@@ -15,68 +16,75 @@ import java.util.Scanner;
 public class Test {
 
     public static void main(String[] args) {
-//	LAB1.PriorityQueue list = new LAB1.PriorityQueue();
-//	Scanner sc = new Scanner(System.in);
-//	int choice;
-//	do {
-//	    System.out.print("Choice: ");
-//	    sc = new Scanner(System.in);
-//	    choice = Integer.parseInt(sc.nextLine());
-//	    switch (choice) {
-//		case 1:
-//		    LAB1.FileWriteRead.readData("user.csv", list);
-//		    break;
-//		case 2:
-//		    System.out.print("Email: ");
-//		    sc = new Scanner(System.in);
-//		    String email = sc.nextLine();
-//		    list.deleteUser(email);
-//		    break;
-//		case 3:
-//		    System.out.print("Email: ");
-//		    sc = new Scanner(System.in);
-//		    email = sc.nextLine();
-//		    sc = new Scanner(System.in);
-//		    System.out.print("Point: ");
-//		    int point = Integer.parseInt(sc.nextLine());
-//		    list.updateUser(email, point);
-//		    LAB1.FileWriteRead.writeData("user.csv", list);
-//		    break;
-//		case 4:
-//		    System.out.print("Email: ");
-//		    sc = new Scanner(System.in);
-//		    email = sc.nextLine();
-//		    list.searchUser(email);
-//		    break;
-//		case 5:
-//		    list.removeTop();
-//		    break;
-//		case 6:
-//		    list.getTop();
-//		case 7:
-//		    System.out.print("Email: ");
-//		    sc = new Scanner(System.in);
-//		    email = sc.nextLine();
-//		    list.displayPointUserEmail(email);
-//		case 8: 
-//		    list.printlist(list);
-//		    break;
-//	    }
-//	} while (choice != 9);
+	PriorityQueue list = new PriorityQueue();
+	Scanner sc = new Scanner(System.in);
+	int choice;
+	do {
+	    System.out.print("Choice: ");
+	    sc = new Scanner(System.in);
+	    choice = Integer.parseInt(sc.nextLine());
+	    switch (choice) {
+		case 1:
+		    FileWriteRead.readData("user.csv", list);
+		    break;
+		case 2:
+		    System.out.print("Email: ");
+		    sc = new Scanner(System.in);
+		    String email = sc.nextLine();
+		    list.deleteUser(email);
+		    break;
+		case 3:
+		    System.out.print("Email: ");
+		    sc = new Scanner(System.in);
+		    email = sc.nextLine();
+		    sc = new Scanner(System.in);
+		    System.out.print("Point: ");
+		    int point = Integer.parseInt(sc.nextLine());
+		    list.updateUser(email, point);
+		    FileWriteRead.writeData("user.csv", list);
+		    break;
+		case 4:
+		    System.out.print("Email: ");
+		    sc = new Scanner(System.in);
+		    email = sc.nextLine();
+		    list.searchUser(email);
+		    break;
+		case 5:
+		    list.removeTop();
+		    break;
+		case 6:
+		    list.getTop();
+		    break;
+		case 7:
+		    System.out.print("Email: ");
+		    sc = new Scanner(System.in);
+		    email = sc.nextLine();
+		    list.displayPointUserEmail(email);
+		    break;
+		case 8: 
+		    list.printlist(list);
+		    break;
+	    }
+	} while (choice != 9);
+//	HTMLValidateAndCount html = new HTMLValidateAndCount();
+//	String tag = sc.nextLine();
+//	System.out.println(html.checkTag(tag));
+	
 
-	Stack list = new Stack();
-	list.push("1");
-	list.push("2");
-	list.push("3");
-	System.out.println(list.top());
-	list.pop();
-	System.out.println(list.top());
-	list.pop();
-	System.out.println(list.pop());
-	System.out.println(list.pop());
-	list.push("4");
-	System.out.println(list.top());
-//	System.out.println(list.size());
-//	System.out.println(list.size());
+//	User user1 = new User("123", 1);
+//	User user2 = new User("436", 2);
+//	User user3 = new User("234", 3);
+//	User user4 = new User("356", 4);
+//	User user5 = new User("174", 5);
+//	User user6 = new User("252", 6);
+//	User user10 = new User("261", 10);
+//	list.add(user1);
+//	list.add(user5);
+//	list.add(user3);
+//	list.add(user6);
+//	list.add(user2);
+//	list.add(user4);
+//	list.add(user10);
+//	list.printlist(list);
     }
 }
