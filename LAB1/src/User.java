727cc000302b5efe4e8af92a983446446c80b309
ACER <ACER@192.168.203.1>
2020-06-10 18:33:59 +0700

    Test game

--- LAB1/src/User.java
+++ LAB1/src/User.java
@@ -9,6 +9,31 @@
  * @author ACER
  */
 public class User {
-    private String email;
-    private String phone;
+    private String email;//user email
+    private long point;//game point
+
+    public User(String email, long point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public User() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPoint() {
+        return point;
+    }
+
+    public void setPoint(long point) {
+        this.point = point;
+    }
+    
 }
