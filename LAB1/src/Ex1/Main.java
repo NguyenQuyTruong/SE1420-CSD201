12cd81b9594170f553d4966adacd859114f96f57
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-10 18:59:42 +0700

    Update Ex1 Main

--- LAB1/src/Ex1/Main.java
+++ LAB1/src/Ex1/Main.java
@@ -30,14 +30,14 @@ public class Main {
         int choice = 0;
         if (args[0].equals("-h")) {
             System.out.println("Exercise 1");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
-            System.out.print("java -jar LAB1.jar <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
+            System.out.print("java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
+            System.out.print("java -jar LAB1.jar 2 <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file");
             return;
         }
 
@@ -73,7 +73,9 @@ public class Main {
             if (args[i].equals("-t")) {
                 choice = 6;
             }
-
+            if (args[3].equals("2")) {
+                choice = 7;
+            }
         }
         switch (choice) {
             case 0: {
@@ -293,7 +295,11 @@ public class Main {
                 choice = 0;
                 break;
             }
-
+            case 7: {
+                
+                choice = 0;
+                break;
+            }
         }
     }
 }
