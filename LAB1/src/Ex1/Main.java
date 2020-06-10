d1e408ac7af7bdd209fa47bd1977803f91170583
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-10 08:06:14 +0700

    Update Ex1 Main

--- LAB1/src/Ex1/Main.java
+++ LAB1/src/Ex1/Main.java
@@ -30,13 +30,14 @@ public class Main {
         int choice = 0;
         if (args[0].equals("-h")) {
             System.out.println("Exercise 1");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
-            System.out.print("java Ex1/Main -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
+            System.out.print("java -cp LAB1.jar Ex1/Main -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
+            return;
         }
 
         for (i = 0; i < args.length; i++) {
