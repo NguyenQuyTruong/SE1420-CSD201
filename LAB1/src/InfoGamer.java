b121ef1c50772d8098aa4580c8c9d940724335f5
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-11 09:45:29 +0700

    This is an information's player.

---
+++ LAB1/src/InfoGamer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nguyen Chi  Cuong
+ */
+public class InfoGamer {
+    private String email;
+    private String point;
+
+    public InfoGamer() {
+    }
+
+    public InfoGamer(String email, String point) {
+        this.email = email;
+        this.point = point;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+    
+    
+    
+}
