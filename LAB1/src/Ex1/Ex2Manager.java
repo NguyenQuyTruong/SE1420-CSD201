92fec610b83688edfed25757d3eb939bfe8821e1
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-11 08:24:25 +0700

    Update Ex2Manager method

---
+++ LAB1/src/Ex1/Ex2Manager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author KHAM
+ */
+public class Ex2Manager {
+    MyStack ms = new MyStack();
+    ArrayList arr = new ArrayList();
+    public void inputStack(String content) {
+        for(char c: content.toCharArray()){
+            if(c = "<"){
+                
+            }
+        }
+    }
+}
