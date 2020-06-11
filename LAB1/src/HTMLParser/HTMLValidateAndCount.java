9dd34b7d0f7f0cc38a8aef7c52781b6d16e120eb
minhv <minhv@DESKTOP-FD4J87R>
2020-06-11 20:17:54 +0700

    Add some function

--- LAB1/src/HTMLParser/HTMLValidateAndCount.java
+++ LAB1/src/HTMLParser/HTMLValidateAndCount.java
@@ -10,8 +10,10 @@ package HTMLParser;
  * @author minhv
  */
 public class HTMLValidateAndCount {
+
     /**
      * Check is open tag or not
+     *
      * @param tag
      * @return true or false
      */
@@ -19,22 +21,25 @@ public class HTMLValidateAndCount {
 	//if it contain "<" and don't contain "</"
 	return tag.contains("<") && !tag.contains("</");
     }
-    
+
     /**
      * Check is close tag or not
+     *
      * @param tag
-     * @return true or false 
+     * @return true or false
      */
     private boolean isCloseTag(String tag) {
 	//if it contain "</"
 	return tag.contains("</");
     }
-    
+
     /**
-     * 
-     * 
+     * If it is open tag and have any information after that like href, id,
+     * class,... Remove it and then add ">" after name tag then return it Else
+     * close tag return it
+     *
      * @param tag
-     * @return 
+     * @return an open tag or a close tag
      */
     public String checkTag(String tag) {
 	if (isOpenTag(tag)) {
@@ -44,9 +49,34 @@ public class HTMLValidateAndCount {
 	    } else {
 		return separateTag[0] + ">";
 	    }
-	}else if (isCloseTag(tag)) {
+	} else if (isCloseTag(tag)) {
 	    return tag;
 	}
 	return null;
+    }
+
+    /**
+     * Convert an open tag to close tag
+     *
+     * @param tag
+     * @return tag
+     */
+    public String converToCloseTag(String tag) {
+	tag = tag.replace("<", "</");
+	return tag;
+    }
+
+    /**
+     * If it is contain in HTML return true, else return false. Khang explains
+     * for me how to identify alone tag like <br>
+     * Thanks for his helping
+     *
+     * @param tag
+     * @param body
+     * @return true or false
+     */
+    public boolean isAloneTag(String tag, String body) {
+	tag = converToCloseTag(tag);
+	return !body.contains(tag);
     }
 }
