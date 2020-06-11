5da345fa1563152c793903864644b3a8d1ad3ff6
ASUS <ASUS@SE140420>
2020-06-11 22:35:23 +0700

    .

--- LAB1/src/DoubleLinkedList.java
+++ LAB1/src/DoubleLinkedList.java
@@ -20,20 +20,54 @@ public class DoubleLinkedList {
         public Node(int point, String email) {
             this.point = point;
             this.email = email;
-            left =right=null;
+            left = right = null;
         }
     }
-    
+
     public Node header;
     private Node pre_header;
     public Node tailer;
-    
-    public void DoubleLinkedList(){
-        Node newNode = new Node(-1,null);//ban dau point va email chua co se la -1 va null
+
+    public void DoubleLinkedList() {
+        Node newNode = new Node(-1, null);//ban dau point va email chua co se la -1 va null
         pre_header = newNode;
-        tailer = new Node(-1,null);
+        tailer = new Node(-1, null);
     }
-    
-    
-  
+
+    public void insert(int point, String email) {
+        Node newNode = new Node(point, email);
+        if (header == null) {
+            header = newNode;
+            header.left = pre_header;
+            header.right = tailer;
+            pre_header.right = header;
+            tailer.left = header;
+        } else {
+            if (newNode.point > header.point) {
+                newNode.right = header;
+                header.left = newNode;
+                pre_header.right = newNode;
+                newNode.left = pre_header;
+                header = newNode; // update lai cai header moi
+            } else if (newNode.point < tailer.left.point) {
+                Node tmp = tailer.left;
+                newNode.right = tailer;
+                tailer.left = newNode;
+                tmp.right = newNode;
+                newNode.left = tmp;
+            } else {
+                Node tmp = header;
+                while (tmp.point>newNode.point) {                    
+                    tmp=tmp.right;
+                }
+                Node preTmp =tmp.left;
+                
+                newNode.right=tmp;
+                tmp.left=newNode;
+                newNode.left=preTmp;
+                preTmp.right=newNode;
+            }
+        }
+    }
+
 }
