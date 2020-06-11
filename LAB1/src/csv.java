fd5a4602aa05c69e22bb128c18ac33ae5fd9bf4c
liemn <liemn@MSI>
2020-06-11 22:59:55 +0700

    123

---
+++ LAB1/src/csv.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liemn
+ */
+public class csv {
+    public void readToQueue(String pathString, DoubleLinkList ls) throws 
+            FileNotFoundException{
+      //  DoubleLinkList ls = new DoubleLinkList();
+        BufferedReader bf = new BufferedReader(new FileReader("user.csv"));
+        String line ="";
+        
+        do {            
+            try {
+                line = bf.readLine();
+                String [] arr = line.split(",");
+                int point = Integer.parseInt(arr[1]);
+                ls.insert(arr[0], point);
+            } catch (Exception e) {
+            }
+        } while (true);
+    }
+    public void writeData(String filePath, DoubleLinkList ls){
+        try {
+            FileWriter writer = new FileWriter(filePath);
+            writer.append("Email, Point\n");
+            DoubleLinkList.Node tmp = ls.head; // chay tu head
+            while(tmp!= ls.tail){
+                writer.append(tmp.email + "," + tmp.point + "\n");
+                tmp = tmp.right;
+                writer.flush(); // luu file
+                writer.close();
+            }
+        } catch (Exception e) {
+        }
+        
+    }
+}
