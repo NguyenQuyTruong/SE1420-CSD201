a978a1e531a1c05ca8ba61a09406a826925c2639
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-11 10:27:34 +0700

    This is an information's player.

--- LAB1/src/InfoGamer.java
+++ LAB1/src/InfoGamer.java
@@ -9,33 +9,55 @@
  * @author Nguyen Chi  Cuong
  */
 public class InfoGamer {
+    /**
+     * Mobile game include: Email, Point of player
+     */
     private String email;
-    private String point;
+    private long point;
 
+    /**
+     * This is constructor without parameter 
+     */
     public InfoGamer() {
     }
 
-    public InfoGamer(String email, String point) {
+    /**
+     * This is constructor parameter to build a game entry with 2 given parameter
+     * @param email
+     * @param point 
+     */
+    public InfoGamer(String email, long point) {
         this.email = email;
         this.point = point;
     }
-
+    /**
+     * it use to return email
+     * @return 
+     */
     public String getEmail() {
         return email;
     }
-
+    
+    /**
+     * it use to set email
+     * @param email 
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public String getPoint() {
+    /**
+     * it use to return point
+     * @return 
+     */
+    public long getPoint() {
         return point;
     }
-
-    public void setPoint(String point) {
+    /**
+     * I use to set point
+     * @param point 
+     */
+    public void setPoint(long point) {
         this.point = point;
     }
-    
-    
-    
+  
 }
