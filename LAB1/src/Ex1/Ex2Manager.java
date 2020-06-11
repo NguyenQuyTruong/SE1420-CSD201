6437e593a5f178855f5a3cce2917b50294f70d44
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-11 09:38:13 +0700

    Update Ex2Manager method

--- LAB1/src/Ex1/Ex2Manager.java
+++ LAB1/src/Ex1/Ex2Manager.java
@@ -5,20 +5,45 @@
  */
 package Ex1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  *
  * @author KHAM
  */
 public class Ex2Manager {
+
     MyStack ms = new MyStack();
     ArrayList arr = new ArrayList();
+    final String opening = "<";
+
     public void inputStack(String content) {
-        for(char c: content.toCharArray()){
-            if(c = "<"){
-                
+        for (char c : content.toCharArray()) {
+            if (opening.indexOf(c) != -1) {
             }
         }
+        
+    }
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        Ex2Manager ex2 = new Ex2Manager();
+        String line = "";
+        String content = "";
+        URL ur = new URL("https://vnexpress.net/");
+
+        HttpsURLConnection urlc = (HttpsURLConnection) ur.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+
+        while ((line = br.readLine()) != null) {
+            content += line;
+        }
+        System.out.print(content);
+        ex2.inputStack(content);
     }
 }
