d58800c9d764e444889558a2f73c2988a3035d7e
liemn <liemn@MSI>
2020-06-11 20:00:09 +0700

    123

---
+++ LAB1/src/DoubleLinkList.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liemn
+ */
+public class DoubleLinkList {
+    private class Node{
+        String email;
+        int point;
+        
+}
+}
