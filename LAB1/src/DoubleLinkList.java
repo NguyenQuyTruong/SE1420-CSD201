8d60e33056ce0171f6b6bdf2d4c5eb624dbbf057
liemn <liemn@MSI>
2020-06-11 20:58:31 +0700

    123

--- LAB1/src/DoubleLinkList.java
+++ LAB1/src/DoubleLinkList.java
@@ -12,6 +12,26 @@ public class DoubleLinkList {
     private class Node{
         String email;
         int point;
+        Node left,right;
+       
+         Node(String email,int point) {
+            this.email=email;
+            this.point=point;
+            left=right=null;
+        }
+    }
+    private Node head;
+    private Node pre_head;
+    private Node tail;
+        public DoubleLinkList(){
+            Node newNode = new Node(null, 0);
+            pre_head = newNode;
+            tail = new Node(null, 0);
+        }
+        public void insert(String email,int point){
+            Node newNode = new Node(email, point);
+            
+        }
         
 }
-}
+
