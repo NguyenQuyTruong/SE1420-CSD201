54600cccdaf8177530ba9d55b2c1de417acd5ab1
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-11 15:19:44 +0700

    Add documentation into MyQueue method

--- LAB1/src/Ex1/MyQueue.java
+++ LAB1/src/Ex1/MyQueue.java
@@ -11,6 +11,9 @@ package Ex1;
  */
 public class MyQueue {
 
+    /**
+     * Create Node method
+     */
     private class Node {
 
         private Entry user;
@@ -38,12 +41,29 @@ public class MyQueue {
         public void setNext(Node next) {
             this.next = next;
         }
-
     }
+    /**
+     * Create Node head = null and Node tail = null Create new variable size = 0
+     */
     private Node head = null;
     private Node tail = null;
     private int size = 0;
 
+    /**
+     * Insert an Entry into Queue by using SinglyLinkedList First, if the queue is
+     * empty, it will insert new Node by using head = newNode and tail = newNode
+     * Size will increase 1 after an enqueue method complete If the point of
+     * Entry is greater than the point of head Node, it will be inserted before
+     * head like "newNode.next = head" and head Node will transfer to this
+     * newNode If the point of Entry is equal the point of head Node, it will be
+     * inserted before head like "newNode.next = head" and swap their point and
+     * email If the point of Entry is less than or equal the point of tail Node,
+     * it will be inserted before head like "tail.next = newNode" and tail Node
+     * will transfer to this newNode Use for loop to insert in the middle of the
+     * queue
+     *
+     * @param e
+     */
     public void enqueue(Entry e) {
         Node newNode = new Node(e);
         Node current = null;
@@ -60,7 +80,7 @@ public class MyQueue {
                 size++;
                 return;
             }
-            if (head.user.getPoint() <= newNode.user.getPoint()) {
+            if (head.user.getPoint() < newNode.user.getPoint()) {
                 newNode.next = head;
                 head = newNode;
                 size++;
@@ -98,36 +118,61 @@ public class MyQueue {
         }
     }
 
+    /**
+     *
+     * @return all point and email of Entry inserted in the queue
+     */
     public String print() {
         Node current = head;
+        int size2 = size;
         String str = current.user.getEmail() + ", " + current.user.getPoint() + "\n";
-        for (int i = 0; i < size - 1; i++) {
+        for (int i = 0; i < size2 - 1; i++) {
             str = str + current.next.user.getEmail() + ", " + current.next.user.getPoint() + "\n";
             current = current.next;
         }
         return str;
     }
 
+    /**
+     * Empty the head Node and transfer it to next Node
+     */
     public void dequeue() {
+        head.user = null;
         head = head.next;
         if (head == null) {
             tail = null;
         }
-        size --;
+        size--;
     }
 
+    /**
+     * @return Entry of the user have highest point in the queue
+     */
     public Entry front() {
         return head.user;
     }
-
+    
+    /**
+     * 
+     * @return head is empty or not
+     */
+    
     public boolean isEmpty() {
         return head == null;
     }
-
+    /**
+     * 
+     * @return size of queue
+     */
     public int size() {
         return size;
     }
-
+    
+    /**
+     * The user insert an Email to find Entry of this email
+     * @param email
+     * @return Entry of this email user or null if it is not exist in queue
+     */
     public Entry search(String email) {
         Node current = head;
         while (current != null) {
@@ -138,17 +183,20 @@ public class MyQueue {
         }
         return null;
     }
-
+    /**
+     * Delete the Entry of the user have the email which is insert in parameter
+     * @param email 
+     */
     public void delete(String email) {
         if (head.user.getEmail().equals(email)) {
             head = head.next;
             System.out.println("Deleted ! ! !");
             delete(email);
-            size --;
-            
+            size--;
+
         }
         Node current = head;
-        
+
         while (current != tail) {
             if (current.next.user.getEmail().equals(email)) {
                 current.next = current.next.next;
@@ -158,7 +206,13 @@ public class MyQueue {
             current = current.next;
         }
     }
-
+    /**
+     * 
+     * @param email
+     * @param New_point
+     * @return The entry of the user have this email and the point is updated
+     * It will return null if this email does not exist in the queue
+     */
     public Entry update(String email, int New_point) {
         Entry user = search(email);
         if (user != null) {
