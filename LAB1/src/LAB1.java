363529a17d5d6a522e4f6376efe8078ba385ab74
liemn <liemn@MSI>
2020-06-11 23:04:41 +0700

    123

--- LAB1/src/LAB1.java
+++ LAB1/src/LAB1.java
@@ -11,5 +11,21 @@
 public class LAB1 {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        DoubleLinkList ls = new DoubleLinkList();
+        ls.insert("aaa", 0);
+        ls.insert("zzz", 9);
+        ls.insert("ccc", 4);
+        ls.insert("bbb", 7);
+        ls.insert("tttt", 5);
+        ls.insert("mmm", 3);
+        ls.print();
+       /* System.out.println("-----------");
+        ls.delete("zzz");
+        ls.delete("mmm");
+        ls.delete("ccc");
+        ls.print();*/
+        System.out.println("-------------------");
+        ls.deleteTop();
+        ls.print();
     }
 }
