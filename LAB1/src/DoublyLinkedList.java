e969ff86ccaa5e3612816726a56efcfc414076a5
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-13 00:26:29 +0700

    to be continued...

--- LAB1/src/DoublyLinkedList.java
+++ LAB1/src/DoublyLinkedList.java
@@ -9,14 +9,15 @@
  * @author Nguyen Chi  Cuong
  */
 public class DoublyLinkedList {
-    // Create a class Node include information's gamer with 2 Node right, left 
-    private class Node {
 
-        InfoGamer info = new InfoGamer(); // new object "info" from InfoGamer.java
-        private String emailSub = info.getEmail(); // to link this emailSub -> email (InfoGamer.java)
-        private long pointSub = info.getPoint();       // and this pointSub -> point (InfoGamer.java)
+    // Create a class Node include information's gamer with 2 Node right, left 
+    public class Node {
+
+         
+        private String emailSub ; // to link this emailSub -> email (InfoGamer.java)
+        private long pointSub ;       // and this pointSub -> point (InfoGamer.java)
         private Node right, left;  // doubly linked list has Node left and right
-        
+
         // Create an contructor for email, point of players with 2 default Node = null
         public Node(String emailSub, long pointSub) {
             this.emailSub = emailSub;
@@ -29,10 +30,56 @@ public class DoublyLinkedList {
      */
     private Node header;
     private Node trailer;
+    private Node prevHeader;
+
+    public DoublyLinkedList() {
+        prevHeader=new Node(null, Integer.MIN_VALUE);
+        trailer=new Node(null, -1);
+    }
     
     
-    
-    
-   
+    /**
+     * Insert email, point
+     * Insert with some situation
+     * @param email
+     * @param point 
+     */
+    public void add(String email, long point) {
+        Node newNode = new Node(email, point);
+        // this situation : list is empty
+        if (header == null) {       
+            newNode.right = trailer;
+            trailer.left = newNode;
+            newNode.left = prevHeader;
+            prevHeader.right = newNode;
+            header = newNode;
+        // this situation: add First   
+        }else if(header.pointSub < newNode.pointSub){
+            
+            newNode.right = header;
+            header.left = newNode;
+            newNode.left = prevHeader;
+            prevHeader.right = newNode;
+            
+            header=newNode;
+        // this situation: add last
+        }else if(header.pointSub > newNode.pointSub){
+            Node tmp = header;
+            newNode.right.right = trailer;
+            trailer.left.left = newNode;
+            newNode.left.left = tmp;
+            tmp.right = newNode;
+            
+            header = newNode;
+        }
+    }
+    public void print(){
+        
+        if(header==null) return;
+            System.out.println("Email"+"\tPoint");
+        for(Node n=prevHeader.right;n!=trailer;n=n.right){
+            System.out.println(""+n.emailSub+"\t"+n.pointSub);
+        }
+    }
 
 }
