e907b57ebdafc0b3dd7579227dfc17700917605c
ACER <ACER@192.168.203.1>
2020-06-12 14:23:10 +0700

    Test game

--- LAB1/src/CSV.java
+++ LAB1/src/CSV.java
@@ -1,3 +1,6 @@
+
+import javax.xml.soap.Node;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,9 @@
  * @author ACER
  */
 public class CSV {
-    
+    Node User;
+    DoubleLinkedList list = new DoubleLinkedList();
+    public void insert(User u){
+        
+    }
 }
