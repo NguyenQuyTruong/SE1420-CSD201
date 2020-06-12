1e93d7601334717df9399fc8a30e3054a4e84483
ACER <ACER@192.168.203.1>
2020-06-12 21:10:19 +0700

    Test game

--- LAB1/src/User.java
+++ LAB1/src/User.java
@@ -12,40 +12,38 @@ import javax.xml.soap.Node;
  * @author ACER
  */
 public class User {
-    /**
-     * create class Node 
-     */
-    public class Node{
         public String email;
         public long point;
         public Node l, r;
-    /**
-     * constructor class Node
+ /**
+     * constructor of user
      * 
      * @param email
      * @param point
      * 
      */
-        public Node(String email, long point, Node l, Node r) {
-            this.email = email;
-            this.point = point;
-            l = r = null;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public long getPoint() {
-            return point;
-        }
-
-        public void setPoint(long point) {
-            this.point = point;
-        } 
+    public User(String email, long point) {
+        this.email = email;
+        this.point = point;
     }
-}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPoint() {
+        return point;
+    }
+
+    public void setPoint(long point) {
+        this.point = point;
+    }
+    
+       
+       }
+
+        
