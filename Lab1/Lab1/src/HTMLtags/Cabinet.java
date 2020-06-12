5da2cba94ef4d69fda5580e008881512cc0cf9d2
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-12 10:27:02 +0700

    add checkTagRegex method

--- Lab1/Lab1/src/HTMLtags/Cabinet.java
+++ Lab1/Lab1/src/HTMLtags/Cabinet.java
@@ -10,13 +10,37 @@ package HTMLtags;
  * @author nhoxr
  */
 public class Cabinet {
-    
+
     /**
      * this method use to check the tag is open or not
+     *
      * @param tag
      * @return true or false if the string input have < or </
      */
     private boolean checkTagOpen(String tag) {
 	return (tag.contains("<") && !tag.contains("</"));
+    }
+
+    private boolean checkTagClose(String tag) {
+	return tag.contains("/");
+    }
+    
+    /**
+     * this method use regex to check the stirng input is the tag or not
+     * @param tag
+     * @return true or false
+     */
+    private boolean checkTagByRegex(String tag) {
+	return tag.matches("^['<''/'A-Za-z0-9]+$");
+    }
+    /**
+     * some special tag like <a ...>, <img ...> don't have the close / character
+     * so this method use to convert them to a normal tag => <a>, <img>,...
+     *
+     * @return normail Tag
+     */
+    private String convertToTag(String allHTML) {
+	String tag = "";
+	return normalTag;
     }
 }
