3553e31abfe2dae7547b1ed03fd4639e1be67f1f
ADMIN <ADMIN@SE141094>
2020-06-13 11:16:04 +0700

    Update Stack Lab 2

---
+++ LAB/src/NguyenLab2/Stack.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NguyenLab2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Stack {
+     int currentElement;
+     String array[];
+     public Stack(int sopt) {
+        array=new String[sopt];
+        currentElement=-1;//mang chua co phan tu
+     }
+}
