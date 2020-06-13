9eb10403be442f7528ac355c3c26b9d2320275b7
liemn <liemn@MSI>
2020-06-13 21:28:09 +0700

    123

--- LAB1/src/LAB1.java
+++ LAB1/src/LAB1.java
@@ -6,40 +6,46 @@ import java.io.FileNotFoundException;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author liemn
  */
 public class LAB1 {
+
     public static void main(String[] args) throws FileNotFoundException {
         DoubleLinkList ls = new DoubleLinkList();
         csv cs = new csv();
         String fileSv = null;
         boolean check = true;
-        for(int i = 0;i<args.length; i++){
-            switch(args[i]){
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
                 case "-r":
-                    cs.readToQueue(args [i+1], ls);
+                    cs.readToQueue(args[i + 1], ls);
                     break;
                 case "-s":
-                    check =false;
-                    fileSv = args[i+1];
+                    check = false;
+                    fileSv = args[i + 1];
                     break;
-                case  "-a":
-                    ls.insert(args[i+1], Integer.parseInt(args[i+2]));
+                case "-a":
+                    ls.insert(args[i + 1], Integer.parseInt(args[i + 2]));
                     break;
                 case "-d":
-                    ls.delete(args[i+1]);
+                    ls.delete(args[i + 1]);
                     break;
                 case "-dt":
                     ls.deleteTop();
+                    break;
                 case "-u":
                     ls.update(args[i + 1], Integer.parseInt(args[i + 2]));
-                                                       
+                    break;
+                case "-t":
+                    ls.geTop();
+                    break;
+                case "-g":
+                    
             }
         }
-        if(check == true){
+        if (check == true) {
             cs.writeData(fileSv, ls);
         }
         ls.print();
