a98789b2c8650a69187631ff3fd0ad92c690a471
ADMIN <ADMIN@SE141094>
2020-06-13 14:04:14 +0700

    Update SovelProblen Class

---
+++ LAB/src/NguyenLab2/SovelProblem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NguyenLab2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SovelProblem {
+    Website myWebsite=new Website();
+    CSV myCsv=new CSV();
+    Stack mystack=new Stack(300);
+}
