f1b31ddd8236e7418411495688def07602d8c25c
SE140279 <SE140279@JackPods>
2020-06-13 23:42:53 +0700

    main

--- LAB1/src/main.java
+++ LAB1/src/main.java
@@ -9,59 +9,59 @@
  * @author SE140279
  */
 public class main {
+
+    public static queue queue;
+
+    public void NewGamer(String email, String point) {
+        try {
+            int checkPoint = Integer.parseInt(point.trim());
+            queue.insert(new manageGamer(email, checkPoint));
+        } catch (NumberFormatException e) {
+            System.out.println("must be integer!!");
+        }
+    }
+public void searchGamer(String email) {
+	manageGamer data = queue.searchUser(email);
+	if (data == null) {
+	    System.out.println("Not found!!");
+	} else {
+	    System.out.println("email: " + email + ", point: " + data.getPoint());
+	}
+    }
+public void deleteGame(String email) {
+	manageGamer data = queue.deleteUser(email);
+	if (data == null) {
+	    System.out.println("Not found!!");
+	} else {
+	    System.out.println("remove " + email + " successful!");
+	}
+    }
+ public void updateGamer(String email, String point) {
+	try {
+	    int newPoint = Integer.parseInt(point.trim());
+	    queue.updateUser(email, newPoint);
+	} catch (NumberFormatException e) {
+	    System.out.println("must be Integer!!");
+	}
+    }
+ public void getTopGamer() {
+	manageGamer data = queue.getTop();
+	System.out.println("A best gamer here:");
+	System.out.println("Email: " + data.getUserEmail() + ", Point: " + data.getPoint());
+    }
+ public void deleteTopGamer() {
+	queue.deleteTop();
+	System.out.println("remove successful!!");
+    }
     public static void main(String[] args) {
-        String of = null;
-        String nf = null;
-        String email = "";
-        int point = 0;
-        int choice = 0;
-        if (args[0].equals("-h")) {
-            System.out.println("Exercise 1");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
-            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
-            System.out.print("java -jar LAB1.jar <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file");
-            return;
-            
+        String input = "";
+        String output = "";
+        try {
+           
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Can't find!");
         }
-
-        for (int i = 0; i < args.length; i++) {
-
-            if (args[i].equals("-r")) {
-                of = args[i + 1];
-            }
-            if (args[i].equals("-s")) {
-                nf = args[i + 1];
-            }
-            if (args[i].equals("-a")) {
-                email = args[i + 1];
-                point = Integer.parseInt(args[i + 2]);
-                choice = 1;
-            }
-            if (args[i].equals("-d")) {
-                email = args[i + 1];
-                choice = 2;
-            }
-            if (args[i].equals("-u")) {
-                email = args[i + 1];
-                point = Integer.parseInt(args[i + 2]);
-                choice = 3;
-            }
-            if (args[i].equals("-dt")) {
-                choice = 4;
-            }
-            if (args[i].equals("-g")) {
-                email = args[i + 1];
-                choice = 5;
-            }
-            if (args[i].equals("-t")) {
-                choice = 6;
-            }
-
-        }
+        System.out.println("Input: " + input);
+        System.out.println("output: " + output);
     }
 }
