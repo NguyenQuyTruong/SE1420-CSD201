f298712141accaf1fc5d18edf6dc9337ac861145
SE140279 <SE140279@JackPods>
2020-06-13 09:40:36 +0700

    main jar

--- LAB1/src/main.java
+++ LAB1/src/main.java
@@ -9,13 +9,59 @@
  * @author SE140279
  */
 public class main {
-
     public static void main(String[] args) {
-        singlyLinkedList list = new singlyLinkedList();
-        list.insert("jack", 200);
-        list.insert("jacky", 300);
-        list.insert("kya", 300);
-        System.out.println("Top 10 Scores: ");
-        list.printList();
+        String of = null;
+        String nf = null;
+        String email = "";
+        int point = 0;
+        int choice = 0;
+        if (args[0].equals("-h")) {
+            System.out.println("Exercise 1");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucuture into csv file\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n");
+            System.out.print("java -jar LAB1.jar -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n");
+            System.out.print("java -jar LAB1.jar <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file");
+            return;
+            
+        }
+
+        for (int i = 0; i < args.length; i++) {
+
+            if (args[i].equals("-r")) {
+                of = args[i + 1];
+            }
+            if (args[i].equals("-s")) {
+                nf = args[i + 1];
+            }
+            if (args[i].equals("-a")) {
+                email = args[i + 1];
+                point = Integer.parseInt(args[i + 2]);
+                choice = 1;
+            }
+            if (args[i].equals("-d")) {
+                email = args[i + 1];
+                choice = 2;
+            }
+            if (args[i].equals("-u")) {
+                email = args[i + 1];
+                point = Integer.parseInt(args[i + 2]);
+                choice = 3;
+            }
+            if (args[i].equals("-dt")) {
+                choice = 4;
+            }
+            if (args[i].equals("-g")) {
+                email = args[i + 1];
+                choice = 5;
+            }
+            if (args[i].equals("-t")) {
+                choice = 6;
+            }
+
+        }
     }
 }
