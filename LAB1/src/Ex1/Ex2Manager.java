cc7a0a1c6c9ae253eff927917f9854f89a6de468
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-13 11:25:09 +0700

    Update ex2Manager Class

--- LAB1/src/Ex1/Ex2Manager.java
+++ LAB1/src/Ex1/Ex2Manager.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import javax.net.ssl.HttpsURLConnection;
 
 /**
@@ -19,32 +20,51 @@ import javax.net.ssl.HttpsURLConnection;
 public class Ex2Manager {
 
     MyStack ms = new MyStack();
+    HashMap hm = new HashMap();
     int count = 0;
-    final char opening = '<';
-    final char closing = '>';
+//    final char opening = '<';
+//    final char closing = '>';
 //    final String space = " ";
 //    final String Mark = "/";
 //    final String Mark2 = "!";
-
+    
     public void inputStack(String content) {
         char c[] = content.toCharArray();
-        String tagString = "";
+        String openTag = "";
+        String closeTag = "";
         for (int i = 0; i < c.length; i++) {
-            tagString = "";
-            if(Character.compare(c[i], opening) == 0){
-                tagString += c[i];
-                for(int j = 1; j < c.length - i; j++){
-                    if((c[i + j] >= 'A' && c[i + j] <= 'Z') || (c[i + j] >= 'a' && c[i + j] <='z')){
-                        tagString += c[i + j];
-                    } 
-                    else{
-                        break;
+            openTag = "";
+            if (c[i] == '<' && c[i + 1] != '/') {
+                if ((c[i + 1] >= 'A' && c[i + 1] <= 'Z') || (c[i + 1] >= 'a' && c[i + 1] <= 'z')) {
+                    openTag += c[i];
+                    for (int j = 1; j < c.length - i; j++) {
+                        if ((c[i + j] >= 'A' && c[i + j] <= 'Z') || (c[i + j] >= 'a' && c[i + j] <= 'z')) {
+                            openTag += c[i + j];
+                        }
+                    }
+                    openTag += '>';
+                    ms.push(openTag);
+                }
+            }
+            if (c[i] == '<' && c[i + 1] == '/') {
+                closeTag = "</";
+                for (int k = 2; k < c.length - i; k++) {
+                    if ((c[i + k] >= 'A' && c[i + k] <= 'Z') || (c[i + k] >= 'a' && c[i + k] <= 'z')) {
+                        closeTag += c[i + k];
                     }
                 }
-                tagString += closing;
-                ms.push(tagString);
+                closeTag += '>';
+                closeTag = closeTag.replace("</", "<");
+                while (!closeTag.equals(ms.pop())) {
+                    if (closeTag.equals(ms.pop())) {
+                        break;
+                    } else {
+                        ms.pop();
+                    }
+                }
             }
         }
+        ms.print();
     }
 
     public static void main(String[] args) throws MalformedURLException, IOException {
