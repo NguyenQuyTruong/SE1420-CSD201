499b5f17667936ad11f1b80a1b775a92ae4d970b
nds72 <nds72@DESKTOP-8QAFAD8>
2020-06-14 00:43:23 +0700

    Merge origin/se140959 into se140959
    Conflicts:
    	Lab201/src/LAB1/csv.java

---
+++ Lab201/src/LAB1_1/Stack.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAB1_1;
+
+/**
+ *
+ * @author nds72
+ */
+public class Stack {
+
+    public static final int CAPACITY = 1000;//default array capacity
+    private String[] data; // generic array used for storage
+    private int t = -1;// index of the top element in stack
+
+    public Stack(int CAPACITY) {
+        data=new String[CAPACITY];
+    }
+    
+    
+    
+    
+}
