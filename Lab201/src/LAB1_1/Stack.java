38f8550f1cba41cbd0fd7c89991297b18b1e5a3a
nds72 <nds72@DESKTOP-8QAFAD8>
2020-06-14 00:48:46 +0700

    Merge origin/se140959 into se140959
    Conflicts:
    	Lab201/src/LAB1/csv.java

--- Lab201/src/LAB1_1/Stack.java
+++ Lab201/src/LAB1_1/Stack.java
@@ -16,12 +16,38 @@ public class Stack {
     private int t = -1;// index of the top element in stack
 
     public Stack(int CAPACITY) {
-        data=new String[CAPACITY];
+        data = new String[CAPACITY];
     }
-    
-    public int size(){
-        return t+1;
+
+    public int size() {
+        return t + 1;
     }
-    
-    
+
+    public boolean isEmty() {
+        return t == -1;
+    }
+
+    public void push(String e) {
+        if (size() == data.length) {
+            throw new IllegalStateException("Stack is full");
+        }
+        data[++t] = e;// increment t before storing new item
+    }
+
+    public String top() {
+        if (isEmty()) {
+            return null;
+        }
+        return data[t];
+    }
+
+    public String pop() {
+        if (isEmty()) {
+            return null;
+        }
+        String answer = data[t];
+        data[t] = null;// dereference to help garbage collection
+        t--;
+        return answer;
+    }
 }
