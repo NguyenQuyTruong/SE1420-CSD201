976ca5107c6f37ffe65d166c0347a8b1dc97ba27
redra <redra@26.12.181.22>
2020-06-13 15:18:58 +0700

    Main For Run

--- LAB1/src/main/java/Main.java
+++ LAB1/src/main/java/Main.java
@@ -1,61 +1,37 @@
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, FileNotFoundException{
-        DoubleLinkedList dll = new DoubleLinkedList();
-        String safeFile = null;
-        boolean save = false;
+    public static PriorityQueue queue;
+    
+    public static void main(String[] args) throws IOException{
+        String inputFile = "";
+        String outputFile = "";
         Main mainStuff = new Main();
-        
-        try {
-            for (int i = 0; i < args.length; i++){
+        for(int i = 0; i < args.length; i ++){
+             try {
                 switch(args[i]){
-                    
+                    case "-r":
+                        queue = new PriorityQueue();
+                        inputFile = args[i + 1];
+                        
+                        break;
+                    case "-a":
+                        break;
+                    case "-u":
+                        break;
+                    case "-dt":
+                        break;
+                    case "-d":
+                        break;
+                    case "-g":
+                        break;
                 }
-            }
-        } catch (Exception e) {
         }
-    }
-    /**
-     * Method use to read file.
-     * 
-     * @param queue
-     * @param fileName 
-     */
-    public void readFile(PriorityQueue queue, String fileName){
-        FileReader fr = null;
-        BufferedReader br = null;
-        try{
-            fr = new FileReader(fileName);
-            br = new BufferedReader(fr);
-            while (br.ready()){
-                String s = br.readLine();
-                String[] arr = s.split(", ");
-                if(!(arr[1].equalsIgnoreCase("point"))){
-                    queue.insertPlayer(new Player(arr[0], Integer.valueOf(arr[1])));
-                }
-            }
-        }catch(IOException e ){
-            System.out.println("---File not exist---");
-        } finally{
-            try {
-                if(fr != null){
-                    fr.close();
-                }
-                if(br != null){
-                    br.close();
-                }
-            } catch (Exception e) {
-                System.out.println("---Something when wrong when reading file---");
-            }
+        catch (Exception e) {
         }
+        }  
     }
+    
 }
