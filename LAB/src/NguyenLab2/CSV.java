80acbaa559520c3922a9a4abfc62d22a3cc3f797
ADMIN <ADMIN@SE141094>
2020-06-13 13:41:49 +0700

    Update CSV

---
+++ LAB/src/NguyenLab2/CSV.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NguyenLab2;
+
+import java.util.Map;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CSV {
+    Map<String, Integer> csv;
+
+}
