678c334c7d780ed4b88f5c0d86c29dbeaeed58dc
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-13 17:25:22 +0700

    add some method

--- Lab1/Lab1/src/HTMLtags/FileDAO.java
+++ Lab1/Lab1/src/HTMLtags/FileDAO.java
@@ -5,19 +5,27 @@
  */
 package HTMLtags;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author nhoxr
  */
 public class FileDAO {
-    Map<String, Integer> data = new LinkedHashMap<>();
+
+    HashMap<String, Integer> data;
+
+    public FileDAO() {
+	data = new HashMap<>();
+    }
 
     /**
-     * this method use to receive tag input and put it to linkedHashMap and count
-     * the time it appear.
+     * this method use to receive tag input and put it to linkedHashMap and
+     * count the time it appear.
+     *
      * @param tag
      */
     public void setTagValue(String tag) {
@@ -25,6 +33,26 @@ public class FileDAO {
 	    data.put(tag, 1);
 	} else { //if the tag exist, get the exist value +1
 	    data.put(tag, data.get(tag) + 1);
+	}
+    }
+
+    public void outputCSV() {
+
+//	Map<String, Integer> result = data.entrySet()
+//		.stream()
+//		.sorted(Map.Entry.comparingByValue())
+//		.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue),
+//			(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	Map<String, Integer> result = data.entrySet()
+		.stream()
+		.sorted(Map.Entry.comparingByValue())
+		.collect(Collectors.toMap(
+			Map.Entry::getKey,
+			Map.Entry::getValue,
+			(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	for (HashMap.Entry<String, Integer> entry : result.entrySet()) {
+	    System.out.println(entry.getKey() + ", " + entry.getValue());
+	    //String format {key: "html", value: 1} to raw string => "html,1\n" => append it to csv file
 	}
     }
 }
