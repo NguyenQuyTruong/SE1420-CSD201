8159d834ab94c6e0d67d8a9c3894867ebd136ce2
Đặng Dương Khâm <66339466+kham-fpt@users.noreply.github.com>
2020-06-13 11:27:16 +0700

    Add Tag method

---
+++ LAB1/src/Ex1/Tag.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex1;
+
+/**
+ *
+ * @author KHAM
+ */
+public class Tag {
+    private String tagName;
+    private int quantity;
+
+    public Tag() {
+    }
+
+    public Tag(String tagName, int quantity) {
+        this.tagName = tagName;
+        this.quantity = quantity;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
