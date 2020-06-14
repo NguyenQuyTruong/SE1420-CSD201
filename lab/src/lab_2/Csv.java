5d52ffaba79e0eab500bbe23eea61977d2cce6e5
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 14:28:54 +0700

    40

--- lab/src/lab_2/Csv.java
+++ lab/src/lab_2/Csv.java
@@ -5,6 +5,9 @@
  */
 package lab_2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedHashMap;
@@ -15,6 +18,10 @@ import java.util.stream.Collectors;
  *
  * @author Khả Thi
  */
+/**
+	 * make a map to store value
+	 * @param map,tag
+	 */
 public class Csv {
     Map<String, Integer> map;
 	
@@ -22,13 +29,17 @@ public class Csv {
 		map = new LinkedHashMap<String, Integer>();
 	}
     public void CheckAndUpdate(String tag){
-        if(map.containsKey(tag)){
+        if(!map.containsKey(tag)){
             map.put(tag, 1);
         }
         else{
-            map.put(tag, map.get(tag)+1);
+            map.put(tag, 1 + map.get(tag));
         }
     }
+    /**
+	 * Path to store output file
+	 * @param path
+	 */
     public void SaveToFile(String path) throws IOException{
         FileWriter writer = new FileWriter(path);
         Map<String, Integer> result = map.entrySet()
@@ -46,3 +57,5 @@ public class Csv {
         writer.close();
     }
 }
+    
+
