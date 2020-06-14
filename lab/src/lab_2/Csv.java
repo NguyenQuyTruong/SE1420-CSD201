559d5e3aef6c0b9487fc3a6be1a3402360097528
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 12:40:23 +0700

    34

--- lab/src/lab_2/Csv.java
+++ lab/src/lab_2/Csv.java
@@ -5,25 +5,45 @@
  */
 package lab_2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author Khả Thi
  */
 public class Csv {
-    Map<String, Integer> data;
+    Map<String, Integer> map;
 	
 	public Csv() {
-		data = new LinkedHashMap<String, Integer>();
+		map = new LinkedHashMap<String, Integer>();
 	}
     public void CheckAndUpdate(String tag){
-        if(data.containsKey(tag)){
-            data.put(tag, 1);
+        if(map.containsKey(tag)){
+            map.put(tag, 1);
         }
         else{
-            data.put(tag, data.get(tag)+1);
+            map.put(tag, map.get(tag)+1);
         }
+    }
+    public void SaveToFile(String path) throws IOException{
+        FileWriter writer = new FileWriter(path);
+        Map<String, Integer> result = map.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByValue())
+        .collect(Collectors.toMap(
+         Map.Entry::getKey, 
+         Map.Entry::getValue, 
+        (oldValue, newValue) -> oldValue, LinkedHashMap::new));;
+         for (Map.Entry<String, Integer> entry : result.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            writer.append(key + "," + value+"\n");
+        }
+        writer.close();
     }
 }
