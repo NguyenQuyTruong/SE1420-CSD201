86a3ba2e2fb9b8e39b5f9b2e92b06e3b1243ba97
Duc Huy <huyvdse140055@fpt.edu.vn>
2020-06-14 07:14:19 +0700

    update double linked list

--- Lab1/Lab1/src/mobileGame/DoubleLinkedList.java
+++ Lab1/Lab1/src/mobileGame/DoubleLinkedList.java
@@ -16,45 +16,45 @@ public class DoubleLinkedList {
     //create class Node
     public class Node {
 
-        private Gamer gamer;
-        private Node right = null;
-        private Node left = null;
+	private Gamer gamer;
+	private Node right = null;
+	private Node left = null;
 
-        //constructor
-        public Node(Gamer gamer, Node right, Node left) {
-            this.gamer = gamer;
-            this.right = right;
-            this.left = left;
-        }
+	//constructor
+	public Node(Gamer gamer, Node right, Node left) {
+	    this.gamer = gamer;
+	    this.right = right;
+	    this.left = left;
+	}
 
-        public Node(Gamer gamer) {
-            this.gamer = gamer;
-        }
+	public Node(Gamer gamer) {
+	    this.gamer = gamer;
+	}
 
-        //getter and setter for Node
-        public Gamer getGamer() {
-            return gamer;
-        }
+	//getter and setter for Node
+	public Gamer getGamer() {
+	    return gamer;
+	}
 
-        public void setGamer(Gamer gamer) {
-            this.gamer = gamer;
-        }
+	public void setGamer(Gamer gamer) {
+	    this.gamer = gamer;
+	}
 
-        public Node getRight() {
-            return right;
-        }
+	public Node getRight() {
+	    return right;
+	}
 
-        public void setRight(Node right) {
-            this.right = right;
-        }
+	public void setRight(Node right) {
+	    this.right = right;
+	}
 
-        public Node getLeft() {
-            return left;
-        }
+	public Node getLeft() {
+	    return left;
+	}
 
-        public void setLeft(Node left) {
-            this.left = left;
-        }
+	public void setLeft(Node left) {
+	    this.left = left;
+	}
     }
 
     //create 2 main node
@@ -64,15 +64,15 @@ public class DoubleLinkedList {
 
     //create DoubleLinkedList constructor
     public DoubleLinkedList() {
-        header = new Node(null, null, null);
-        trailer = new Node(null, null, header);
-        header.right = trailer;
+	header = new Node(null, null, null);
+	trailer = new Node(null, null, header);
+	header.right = trailer;
     }
 
     //check the list is empty or not
     public boolean isEmpty() {
-        return size == 0;
-        //if size = 0, list is empty
+	return size == 0;
+	//if size = 0, list is empty
     }
 
     /**
@@ -81,7 +81,7 @@ public class DoubleLinkedList {
      * @return size
      */
     public int size() {
-        return size;
+	return size;
     }
 
     /**
@@ -90,10 +90,10 @@ public class DoubleLinkedList {
      * @param newNode
      */
     public void addFirst(Node newNode) {
-        header.right.left = newNode;
-        newNode.right = header.right;
-        newNode.left = header;
-        header.right = newNode;
+	header.right.left = newNode;
+	newNode.right = header.right;
+	newNode.left = header;
+	header.right = newNode;
     }
 
     /**
@@ -102,10 +102,10 @@ public class DoubleLinkedList {
      * @param newNode
      */
     public void addLast(Node newNode) {
-        trailer.left.right = newNode;
-        newNode.left = trailer.left;
-        newNode.right = trailer;
-        trailer.left = newNode;
+	trailer.left.right = newNode;
+	newNode.left = trailer.left;
+	newNode.right = trailer;
+	trailer.left = newNode;
     }
 
     /**
@@ -115,37 +115,37 @@ public class DoubleLinkedList {
      * @param data
      */
     public void addBetween(Gamer data) {
-        //create new node to add
-        Node newbie = new Node(data, trailer, header);
-        int point = data.getPoint();
-        //if list is empty
-        if (isEmpty()) {
-            header.right = newbie;
-            newbie.left = header;
-            newbie.right = trailer;
-            trailer.left = newbie;
-        } else {
-            int pointOfHeader = header.right.getGamer().getPoint();
-            int pointOfTrailer = trailer.left.getGamer().getPoint();
-            //if input point < point of trailer, add new trailer
-            if (point < pointOfTrailer) {
-                addLast(newbie);
-            } //if input point > point of header, add new header
-            else if (point > pointOfHeader) {
-                addFirst(newbie);
-            } //normal case, go through the list and compare every node until get right position
-            else {
-                Node currentNode = header.right;
-                while (currentNode.getGamer().getPoint() > newbie.getGamer().getPoint()) {
-                    currentNode = currentNode.right;
-                }
-                currentNode.left.right = newbie;
-                newbie.right = currentNode;
-                newbie.left = currentNode.left;
-                currentNode.left = newbie;
-            }
-        }
-        size++;
+	//create new node to add
+	Node newbie = new Node(data, trailer, header);
+	int point = data.getPoint();
+	//if list is empty
+	if (isEmpty()) {
+	    header.right = newbie;
+	    newbie.left = header;
+	    newbie.right = trailer;
+	    trailer.left = newbie;
+	} else {
+	    int pointOfHeader = header.right.getGamer().getPoint();
+	    int pointOfTrailer = trailer.left.getGamer().getPoint();
+	    //if input point < point of trailer, add new trailer
+	    if (point < pointOfTrailer) {
+		addLast(newbie);
+	    } //if input point > point of header, add new header
+	    else if (point > pointOfHeader) {
+		addFirst(newbie);
+	    } //normal case, go through the list and compare every node until get right position
+	    else {
+		Node currentNode = trailer.left;
+		while (currentNode.getGamer().getPoint() < newbie.getGamer().getPoint()) {
+		    currentNode = currentNode.left;
+		}
+		currentNode.right.left = newbie;
+		newbie.left = currentNode;
+		newbie.right = currentNode.right;
+		currentNode.right = newbie;
+	    }
+	}
+	size++;
     }
 
     /**
@@ -155,12 +155,12 @@ public class DoubleLinkedList {
      * @return info of node
      */
     private Gamer remove(Node node) {
-        Node left = node.left;
-        Node right = node.right;
-        left.setRight(right);
-        right.setLeft(left);
-        size--;
-        return node.getGamer();
+	Node left = node.left;
+	Node right = node.right;
+	left.setRight(right);
+	right.setLeft(left);
+	size--;
+	return node.getGamer();
     }
 
     /**
@@ -169,34 +169,34 @@ public class DoubleLinkedList {
      * @return Gamer
      */
     public Gamer first() {
-        if (isEmpty()) {
-            return null;
-        }
-        return header.getRight().getGamer();
+	if (isEmpty()) {
+	    return null;
+	}
+	return header.getRight().getGamer();
     }
 
     /**
      * this method will remove first gamer of the list
      */
     public void removeFirst() {
-        if (isEmpty()) {
-            System.out.println("The list is empty!!");
-        } else {
-            remove(header.getRight());
-        }
+	if (isEmpty()) {
+	    System.out.println("The list is empty!!");
+	} else {
+	    remove(header.getRight());
+	}
     }
 
     /**
      * this method will print all gamer in the list by go through the list
      */
     public void printList() {
-        //begin in the first node after header
-        Node currentNode = header.getRight();
-        //this loop will scan every node and it will stop until meet the trailer
-        while (currentNode != trailer) {
-            System.out.println(currentNode.getGamer() + "\n");
-            currentNode = currentNode.getRight();
-        }
+	//begin in the first node after header
+	Node currentNode = header.getRight();
+	//this loop will scan every node and it will stop until meet the trailer
+	while (currentNode != trailer) {
+	    System.out.println(currentNode.getGamer() + "\n");
+	    currentNode = currentNode.getRight();
+	}
     }
 
     /**
@@ -208,12 +208,12 @@ public class DoubleLinkedList {
      * @return gamer
      */
     public Gamer searchGamerByEmail(String email) {
-        for (Node n = header.right; n != trailer; n = n.right) {
-            if (n.getGamer().getEmail().contentEquals(email)) {
-                return n.getGamer();
-            }
-        }
-        return null;
+	for (Node n = header.right; n != trailer; n = n.right) {
+	    if (n.getGamer().getEmail().contentEquals(email)) {
+		return n.getGamer();
+	    }
+	}
+	return null;
     }
 
     /**
@@ -225,12 +225,12 @@ public class DoubleLinkedList {
      * @return Node
      */
     public Node searchGamerToDelete(String email) {
-        for (Node n = header.right; n != trailer; n = n.right) {
-            if (n.getGamer().getEmail().contentEquals(email)) {
-                return n;
-            }
-        }
-        return null;
+	for (Node n = header.right; n != trailer; n = n.right) {
+	    if (n.getGamer().getEmail().contentEquals(email)) {
+		return n;
+	    }
+	}
+	return null;
     }
 
     /**
@@ -240,13 +240,12 @@ public class DoubleLinkedList {
      * @return Gamer has been remove
      */
     public Gamer removeNode(String email) {
-        Node delNode = searchGamerToDelete(email);
-        if (delNode != null) {
-            return remove(delNode);
-        }
-        return null;
+	Node delNode = searchGamerToDelete(email);
+	if (delNode != null) {
+	    return remove(delNode);
+	}
+	return null;
     }
-
 
     /**
      * this method will get the list and write all the Gamer info to CSV file
@@ -255,24 +254,24 @@ public class DoubleLinkedList {
      * @throws java.io.IOException
      */
     public void writeToCSVfile(String fileName) throws IOException {
-        FileWriter fr = null;
-        try {
-            fr = new FileWriter(fileName);
+	FileWriter fr = null;
+	try {
+	    fr = new FileWriter(fileName);
 	    fr.append("Email, Point\n");
-            for (Node n = header.right; n != trailer; n = n.right) {
-                String data = String.format("%s, %d\n", n.getGamer().getEmail(), n.getGamer().getPoint());
-                fr.append(data);
-            }
-        } catch (IOException e) {
-            System.out.println("Something wrong with writing to file!!");
-        } finally {
-            try {
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                System.out.println("File not exist!!");
-            }
-        }
+	    for (Node n = header.right; n != trailer; n = n.right) {
+		String data = String.format("%s, %d\n", n.getGamer().getEmail(), n.getGamer().getPoint());
+		fr.append(data);
+	    }
+	} catch (IOException e) {
+	    System.out.println("Something wrong with writing to file!!");
+	} finally {
+	    try {
+		if (fr != null) {
+		    fr.close();
+		}
+	    } catch (IOException e) {
+		System.out.println("File not exist!!");
+	    }
+	}
     }
 }
