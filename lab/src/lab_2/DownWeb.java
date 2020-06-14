be17263b1cebb92ff02ef488598539a790e62fec
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 12:19:42 +0700

    30

--- lab/src/lab_2/DownWeb.java
+++ lab/src/lab_2/DownWeb.java
@@ -5,18 +5,25 @@
  */
 package lab_2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 
 /**
  *
  * @author Khả Thi
  */
 public class DownWeb {
-    public static void Download(String url) throws MalformedURLException{
+    public static void Download(String url) throws MalformedURLException, IOException{
        URL urld = new URL(url);
        String Path;
-       
+       BufferedReader reader =  new BufferedReader(new InputStreamReader(url.openStream()));
+       BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\page.html"));
     }
     
 }
