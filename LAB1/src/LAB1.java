e65b7cbded74605eb37c0e0990b7aea669a5b7b0
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-14 12:48:45 +0700

    to be continued...

--- LAB1/src/LAB1.java
+++ LAB1/src/LAB1.java
@@ -1,3 +1,6 @@
+
+import java.io.IOException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +12,20 @@
  * @author Nguyen Chi  Cuong
  */
 public class LAB1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        DoublyLinkedList d = new DoublyLinkedList();
+        
+        d.add("H", 5);
+        d.add("LL", 1);
+        d.add("k", 2);
+        d.add("jj", 3);
+        d.add("jaj", 9);
+        d.update("H", 1000);
+                
+//        d.print();
+        FileCsv fc=new FileCsv();
+        fc.readDataFormFileCsv("D:\\CSD\\lab1\\SE1420-CSD201\\LAB1\\user.csv", d);
+        fc.write("csvvv.csv", d);
+        
     }
 }
