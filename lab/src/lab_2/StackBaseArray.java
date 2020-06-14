1bd4f179f7cb177df77dabdc5332d1479b13d946
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 12:00:23 +0700

    26

---
+++ lab/src/lab_2/StackBaseArray.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_2;
+
+/**
+ *
+ * @author Khả Thi
+ */
+public class StackBaseArray implements Stack{
+    int Size ;
+    String Array[];
+    public StackBaseArray(){
+        Size = -1;
+    }
+    @Override
+    
+}
