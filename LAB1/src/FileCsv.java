be570e5f7be0269e9d007d90eca5d9dfdf028af1
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-14 11:49:18 +0700

    to be continued...

---
+++ LAB1/src/FileCsv.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nguyen Chi  Cuong
+ */
+public class FileCsv {
+    public void readDataFormFileCsv(String path, DoublyLinkedList dll) throws FileNotFoundException, IOException{
+        
+        BufferedReader read = new BufferedReader(new FileReader(path));
+        String data;
+        while((data = read.readLine()) != null ){
+            String [] array = data.split(",");
+            
+            dll.add(array[0], Long.parseLong(array[1].trim()));
+        }
+    }
+    public static void write(String path, DoublyLinkedList dll) throws IOException{
+        FileWriter fileWriter = new FileWriter(path);
+        fileWriter.append("Email, Point\n");
+        DoublyLinkedList.Node tmp = dll.header;
+        
+    }
+}
