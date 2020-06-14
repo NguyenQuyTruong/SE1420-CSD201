1bbfa6e488045b210a173fddd7c4b3412a3fc2d2
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 11:14:04 +0700

    22

--- lab/src/lab/Lab.java
+++ lab/src/lab/Lab.java
@@ -9,6 +9,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +21,7 @@ public class Lab {
     /**
      * @param args the command line arguments
      */
-           PriorityQueue queue;
+           static PriorityQueue queue;
            String filename;
     public void Save() throws IOException {
 		try {
@@ -38,7 +40,24 @@ public class Lab {
 	}
     public static void main(String[] args) {
         // TODO code application logic here
- 
+        boolean save = false;
+        String Path = null;
+        for (int i = 0; i < args.length; i++) {
+            switch(args[i]){
+                case "r":
+            {
+                try {
+                    queue = new PriorityQueue();
+                    Csv.ReadCsv(Path, queue);
+                } catch (IOException ex) {
+                    Logger.getLogger(Lab.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+                    
+                    
+            }
+        }
+        
         
     }
     
