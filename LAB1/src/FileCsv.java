c42d152200774c0030880c1c90ec8b1850928140
Nguyen Chi  Cuong <Nguyen Chi  Cuong@SE140042>
2020-06-14 12:24:14 +0700

    to be continued...

--- LAB1/src/FileCsv.java
+++ LAB1/src/FileCsv.java
@@ -16,6 +16,7 @@ import java.io.IOException;
  * @author Nguyen Chi  Cuong
  */
 public class FileCsv {
+    
     public void readDataFormFileCsv(String path, DoublyLinkedList dll) throws FileNotFoundException, IOException{
         
         BufferedReader read = new BufferedReader(new FileReader(path));
@@ -26,10 +27,10 @@ public class FileCsv {
             dll.add(array[0], Long.parseLong(array[1].trim()));
         }
     }
-    public static void write(String path, DoublyLinkedList dll) throws IOException{
+    public void write(String path, DoublyLinkedList dll) throws IOException{
         FileWriter fileWriter = new FileWriter(path);
-        fileWriter.append("Email, Point\n");
+        fileWriter.append("Email,"+ " Point"+"\n");
         DoublyLinkedList.Node tmp = dll.header;
-        
+        while(tmp != dll.trailer )
     }
 }
