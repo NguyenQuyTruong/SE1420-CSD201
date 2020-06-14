c029f7ad2a9555353500be97f97ba6cfc550b0d1
ACER <ACER@192.168.203.1>
2020-06-14 21:44:28 +0700

    Test game

--- LAB1/src/CSV.java
+++ LAB1/src/CSV.java
@@ -22,9 +22,7 @@ public class CSV {
             + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\r\n"
             + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\r\n"
             + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\r\n"
-            + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\r\n"
-            + "java -jar LAB1.jar 2 <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file.";
-    
+            + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\r\n";    
     public static void main(String[] args) {
         CSV csv = new CSV();
         if(args.length > 0){
