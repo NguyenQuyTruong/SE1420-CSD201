2ad81443f17603dde0ed03293be0dae7202d4e9c
nds72 <nds72@DESKTOP-8QAFAD8>
2020-06-14 19:07:40 +0700

    Merge origin/se140959 into se140959
    Conflicts:
    	Lab201/src/LAB1/csv.java

--- Lab201/src/LAB1/Lab201.java
+++ Lab201/src/LAB1/Lab201.java
@@ -30,6 +30,17 @@ public class Lab201 {
                 System.out.println("Bai 1");
                 for (int i = 0; i < args.length; i++) {
                     switch (args[i]) {
+                        case "-h":
+                            System.out.println("Help: \n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>>: Problem 1, read the user csv file and save the data strucutre into csv file\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -a <<email>> <<point>>: Problem 1, add a new user into the data strucutre and save to new csv file\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -d <<email>>: Problem 1, delete a user in the data strucutre and save to new csv file\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -u <<email>> <<new_point>>: Problem 1, update new point for user in the data strucutre and save to new csv file\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -s <<new_user_CSV_file>> -dt: Problem 1, delete the top user from the data strucutre and save to new csv file\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -g <<email>>: Problem 1, get the point of user from the data strucutre\n"
+                                    + "java -jar LAB1.jar 1 -r <<user_CSV_file>> -t: Problem 1, get the point of the top user from the data strucutre\n"
+                                    + "java -jar LAB1.jar 2 <<URL-of-website>> <<output-CSV-file>>: Problem 2, read html info from a URL, save all tag information into the CSV output file");
+                            break;
                         case "-r":
                             lab.readDataQueue(args[i + 1], doublyLinkedList);
                             break;
