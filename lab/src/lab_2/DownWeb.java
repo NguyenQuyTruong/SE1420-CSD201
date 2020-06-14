6d664512074b9da972130cdb164c7ebe442e2860
Khả Thi <Kh Thi@169.254.71.86>
2020-06-14 12:16:51 +0700

    29

---
+++ lab/src/lab_2/DownWeb.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_2;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Khả Thi
+ */
+public class DownWeb {
+    public static void Download(String url) throws MalformedURLException{
+       URL urld = new URL(url);
+       String Path;
+       
+    }
+    
+}
