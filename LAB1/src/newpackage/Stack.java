74cb42b079d5bb303ac616b92b9904bb5028f1be
liemn <liemn@MSI>
2020-06-14 14:51:03 +0700

    stack

--- LAB1/src/newpackage/Stack.java
+++ LAB1/src/newpackage/Stack.java
@@ -10,5 +10,41 @@ package newpackage;
  * @author liemn
  */
 public class Stack {
-    
+    int top = 0;
+    String [] stackTag;
+    public final int sz = 1000;
+    public Stack(int sz){
+        stackTag = new String[sz];
+    }
+    public int size(){
+        return top;
+    }
+    public boolean  isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        return false;
+    }
+    public void push(String htmlTag){
+        if(size()==stackTag.length){
+            System.out.println("Stack Full");
+        }else{
+            stackTag[++top] = htmlTag;
+        }
+    }
+    public String top(){
+        if(isEmpty()){
+            return null;
+        }
+        return stackTag[top];
+    }
+    public String pop(){
+        if(isEmpty()){
+            return null;
+        }
+        String htmlTag = stackTag[top];
+        stackTag[top] = null;
+        top --;
+        return htmlTag;
+    }   
 }
